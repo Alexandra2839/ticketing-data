842b0afcbcdc7ad3aeed6ea6a59b9754a01add3e
Sasha <alexandra.aslobodian@gmail.com>
2023-04-20 13:10:29 -0400

    implemented update method for /delete endpoint

--- src/main/java/com/learn/repository/UserRepository.java
+++ src/main/java/com/learn/repository/UserRepository.java
@@ -3,7 +3,12 @@ package com.learn.repository;
 import com.learn.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String username);
+
+    @Transactional
+    void deleteByUserName(String username);
 }
--- src/main/java/com/learn/service/impl/UserServiceImpl.java
+++ src/main/java/com/learn/service/impl/UserServiceImpl.java
@@ -46,6 +46,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public void deleteByUserName(String username) {
 
+        userRepository.deleteByUserName(username);
+
     }
 
     @Override
