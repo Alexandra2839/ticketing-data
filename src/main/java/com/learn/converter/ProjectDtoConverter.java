336bd5183bb1e0b51667e460655b197e83dd0b87
Sasha <alexandra.aslobodian@gmail.com>
2023-04-22 21:30:20 -0400

    implemented ProjectDtoConverter

--- src/main/java/com/learn/converter/ProjectDtoConverter.java
+++ src/main/java/com/learn/converter/ProjectDtoConverter.java
@@ -1,32 +1,30 @@
 package com.learn.converter;
 
 import com.learn.dto.ProjectDTO;
+import com.learn.service.ProjectService;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 //@ConfigurationPropertiesBinding
 public class ProjectDtoConverter implements Converter<String, ProjectDTO> {
-    @Override
-    public ProjectDTO convert(String source) {
-        return null;
+
+
+    ProjectService projectService;
+
+    public ProjectDtoConverter(ProjectService projectService) {
+        this.projectService = projectService;
     }
 
-//    ProjectService projectService;
-//
-//    public ProjectDtoConverter(ProjectService projectService) {
-//        this.projectService = projectService;
-//    }
-//
-//    @Override
-//    public ProjectDTO convert(String source) {
-//
-//        if (source == null || source.equals("")) {
-//            return null;
-//        }
-//
-//        return projectService.findById(source);
-//
-//    }
+    @Override
+    public ProjectDTO convert(String source) {
+
+        if (source == null || source.equals("")) {
+            return null;
+        }
+
+        return projectService.getByProjectCode(source);
+
+    }
 
 }
