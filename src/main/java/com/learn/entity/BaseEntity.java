d6fd29f5a7afd5f5ea0f8a80ef5815935b959084
Sasha <alexandra.aslobodian@gmail.com>
2023-04-22 21:27:04 -0400

    added nullable/ updatable to fields

--- src/main/java/com/learn/entity/BaseEntity.java
+++ src/main/java/com/learn/entity/BaseEntity.java
@@ -8,21 +8,38 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@MappedSuperclass
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private Boolean isDeleted = false;
-    @Column(columnDefinition = "TIMESTAMP")
+
+    @Column(nullable = false,updatable = false)
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
-    @Column(columnDefinition = "TIMESTAMP")
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
+
+
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
+
 
 
 
