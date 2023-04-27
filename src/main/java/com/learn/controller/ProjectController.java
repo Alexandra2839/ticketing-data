d12dbd0b06fcae6bcaf45b2dd08735fda68cc541
Sasha <alexandra.aslobodian@gmail.com>
2023-04-27 14:06:06 -0400

    implemented /project-status endpoint

--- src/main/java/com/learn/controller/ProjectController.java
+++ src/main/java/com/learn/controller/ProjectController.java
@@ -93,18 +93,18 @@ public class ProjectController {
         return "redirect:/project/create";
 
     }
-//
-//    @GetMapping("/manager/project-status")
-//    public String getProjectByManager(Model model) {
-//
-//        UserDTO manager = userService.findById("john@cydeo.com");
-//        List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
-//
-//        model.addAttribute("projects", projects);
-//
-//        return "/manager/project-status";
-//
-//    }
+
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model) {
+
+        //UserDTO manager = userService.findByUserName("harold@manager.com");
+        List<ProjectDTO> projects = projectService.listAllProjectDetails();
+
+        model.addAttribute("projects", projects);
+
+        return "/manager/project-status";
+
+    }
 //
 //    @GetMapping("/manager/complete/{projectCode}")
 //    public String managerCompleteProject(@PathVariable("projectCode") String projectCode) {
