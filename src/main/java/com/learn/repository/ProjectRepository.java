49354f8538a78a93ed92e2ef0b64f57f684ea165
Sasha <alexandra.aslobodian@gmail.com>
2023-04-27 14:08:51 -0400

    added findByAssignedManager query

--- src/main/java/com/learn/repository/ProjectRepository.java
+++ src/main/java/com/learn/repository/ProjectRepository.java
@@ -1,9 +1,14 @@
 package com.learn.repository;
 
 import com.learn.entity.Project;
+import com.learn.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     Project findByProjectCode(String code);
+
+    List<Project> findByAssignedManager(User manager);
 }
