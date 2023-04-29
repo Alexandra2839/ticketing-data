2e5e1b24c1a06a9ce642c652ccdba29b25a9c115
Sasha <alexandra.aslobodian@gmail.com>
2023-04-28 21:41:50 -0400

    modified delete method based on uncompleted tasks or projects

--- src/main/java/com/learn/service/impl/UserServiceImpl.java
+++ src/main/java/com/learn/service/impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.learn.service.impl;
 
+import com.learn.dto.ProjectDTO;
+import com.learn.dto.TaskDTO;
 import com.learn.dto.UserDTO;
 import com.learn.entity.User;
 import com.learn.mapper.UserMapper;
 import com.learn.repository.UserRepository;
+import com.learn.service.ProjectService;
+import com.learn.service.TaskService;
 import com.learn.service.UserService;
+
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -15,25 +21,28 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
     private final UserMapper userMapper;
+    private final ProjectService projectService;
+    private final TaskService taskService;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper, @Lazy ProjectService projectService, @Lazy TaskService taskService) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.projectService = projectService;
+        this.taskService = taskService;
     }
 
     @Override
     public List<UserDTO> listAllUsers() {
 
-        List<User> userList = userRepository.findAll(Sort.by("firstName"));
+        List<User> userList = userRepository.findAllByIsDeletedOrderByFirstNameDesc(false);
 
         return userList.stream().map(userMapper::convertToDto).collect(Collectors.toList());
     }
 
     @Override
     public UserDTO findByUserName(String username) {
-        return userMapper.convertToDto(userRepository.findByUserName(username));
+        return userMapper.convertToDto(userRepository.findByUserNameAndIsDeleted(username, false));
     }
 
     @Override
@@ -53,7 +62,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDTO update(UserDTO user) {
         //find current uset
-        User user1 = userRepository.findByUserName(user.getUserName());
+        User user1 = userRepository.findByUserNameAndIsDeleted(user.getUserName(), false);
 
         User convertedUser = userMapper.covertToEntity(user);
         //set id to the converted object
@@ -67,17 +76,40 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void delete(String username) {
-        User user =  userRepository.findByUserName(username);
-        user.setIsDeleted(true);
+        User user =  userRepository.findByUserNameAndIsDeleted(username, false);
 
-        userRepository.save(user);
+        if (checkIfUserCanBeDeleted(user)) {
+            user.setIsDeleted(true);
+            user.setUserName(user.getUserName() + "-" + user.getId());
+            userRepository.save(user);
+        }
+
     }
 
     @Override
     public List<UserDTO> listAllByRole(String role) {
-        List<User> users = userRepository.findByRoleDescriptionIgnoreCase(role);
+        List<User> users = userRepository.findByRoleDescriptionIgnoreCaseAndIsDeleted(role, false);
 
 
         return users.stream().map(userMapper::convertToDto).collect(Collectors.toList());
     }
+
+    private boolean checkIfUserCanBeDeleted(User user){
+
+        switch(user.getRole().getDescription()){
+            case "Manager":
+                List<ProjectDTO> projectDTOList = projectService.listAllNonCompletedByAssignedManager(userMapper.convertToDto(user));
+                return projectDTOList.size() ==0;
+
+            case "Employee":
+                List<TaskDTO> taskDTOList = taskService.listAllNonCompletedByAssignedEmployee(userMapper.convertToDto(user));
+                return taskDTOList.size() ==0;
+
+            default:
+                return true;
+        }
+
+
+    }
+
 }
